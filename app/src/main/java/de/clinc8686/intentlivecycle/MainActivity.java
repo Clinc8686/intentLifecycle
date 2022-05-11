d93dc2cca378c0ddb2ae1bdfd432d295eb6e0009
Mario <mario1.lanpert@gmail.com>
2022-05-11 11:48:36 +0200

    Initial commit

---
+++ app/src/main/java/de/clinc8686/intentlivecycle/MainActivity.java
@@ -0,0 +1,14 @@
+package de.clinc8686.intentlivecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
