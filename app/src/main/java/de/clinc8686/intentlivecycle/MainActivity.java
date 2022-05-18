cac40be2513785ac330e86975447170251e3160a
Mario <mario1.lanpert@gmail.com>
2022-05-18 09:22:57 +0200

    Aufgabe 2 bearbeitet, just added comments

--- app/src/main/java/de/clinc8686/intentlivecycle/MainActivity.java
+++ app/src/main/java/de/clinc8686/intentlivecycle/MainActivity.java
@@ -17,10 +17,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    //Hier wäre es besser auf null/empty zu prüfen, statt in der second_Activity. Dann könnte man direkt den passende string übertragen, ob leer ist
     public void sendText(View view) {
         TextView sendingText = (TextView) findViewById(R.id.intentText);
         String sendingTextString = sendingText.getText().toString();
-        Intent secondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);
+        Intent secondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);  //Expliziter Intent verwendet, anderes (nicht hier) wäre Impliziter Intent. Implizit ist für Standardapps wie Browser, sodass gefragt wird welche App man verwenden möchte
         secondActivityIntent.putExtra("extraString", sendingTextString);
         startActivity(secondActivityIntent);
     }
