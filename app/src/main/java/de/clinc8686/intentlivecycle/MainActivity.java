02b8e42de9e253a4ad8a56ed7c0abc6eb41ae5cd
Clinc8686 <mario1.lanpert@gmail.com>
2022-05-19 11:16:34 +0200

    Uebung 4, Aufgabe 4 finished

--- app/src/main/java/de/clinc8686/intentlivecycle/MainActivity.java
+++ app/src/main/java/de/clinc8686/intentlivecycle/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Lifecycle Main", "Create");
     }
 
 
@@ -22,29 +23,66 @@ public class MainActivity extends AppCompatActivity {
     public void sendText(View view) {
         TextView sendingText = (TextView) findViewById(R.id.intentText);
         String sendingTextString = sendingText.getText().toString();
+        if (sendingText.length() <= 0) {
+            sendingTextString = "Empty String!";
+        }
+
         Intent secondActivityIntent = new Intent(MainActivity.this, SecondActivity.class);  //Expliziter Intent verwendet, anderes (nicht hier) wäre Impliziter Intent. Implizit ist für Standardapps wie Browser, sodass gefragt wird welche App man verwenden möchte
         secondActivityIntent.putExtra("extraString", sendingTextString);
-        //startActivity(secondActivityIntent);
-        startActivityForResult(secondActivityIntent, RESULT_OK);
-
+        startActivityForResult(secondActivityIntent, RESULT_CANCELED);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        Log.e("a", "b");
         super.onActivityResult(requestCode, resultCode, data);
         TextView resultText = (TextView) findViewById(R.id.resultCode);
-        resultText.setText("Message received! Result code: " + resultText);
+        resultText.setText("Message received! Request code: " + requestCode);
 
         EditText receivedTextfield = findViewById(R.id.intentText);
 
         Intent receiver = getIntent();
-        String receivedString = receiver.getStringExtra("extraString");
+        String receivedString = data.getStringExtra("extraString");
         if (receivedString.length() == 0) {
             resultText.setText("empty String!");
         }
 
         receivedTextfield.setText(receivedString);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Lifecycle Main", "Start");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Lifecycle Main", "Pause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Lifecycle Main", "Restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle Main", "Resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Lifecycle Main", "Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle Main", "Destroy");
     }
 }
--- app/src/main/java/de/clinc8686/intentlivecycle/SecondActivity.java
+++ app/src/main/java/de/clinc8686/intentlivecycle/SecondActivity.java
@@ -12,16 +12,13 @@ public class SecondActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("Lifecycle Second", "Create");
         setContentView(R.layout.second_activity_main);
         EditText receivedTextfield = findViewById(R.id.receivedText);
         TextView textHint = findViewById(R.id.textHint);
 
         Intent receiver = getIntent();
         String receivedString = receiver.getStringExtra("extraString");
-        if (receivedString.length() == 0) {
-            textHint.setText("empty String!");
-        }
-
         receivedTextfield.setText(receivedString);
     }
 
@@ -30,7 +27,43 @@ public class SecondActivity extends Activity {
         String sendingTextString = sendingText.getText().toString();
         Intent mainActivityIntent = new Intent();
         mainActivityIntent.putExtra("extraString", sendingTextString);
-        setResult(RESULT_OK, mainActivityIntent);
+        setResult(RESULT_CANCELED, mainActivityIntent);
         finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Lifecycle Second", "Start");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("Lifecycle Second", "Pause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("Lifecycle Second", "Restart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle Second", "Resume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("Lifecycle Second", "Stop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("Lifecycle Second", "Destroy");
     }
 }
